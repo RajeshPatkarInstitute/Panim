ab9caebc80243bd8757a435429d6af5e35837458
Archan Patkar <archanpatkar@gmail.com>
2018-05-18 03:41:03 +0530

    Update

--- Panim.py
+++ Panim.py
@@ -1,3 +1,5 @@
+import time
+
 BLACK = 0
 RED = 1
 GREEN = 2
@@ -8,23 +10,26 @@ CYAN = 6
 WHITE = 7
 
 def clrscr():
-    print("\033[2J",end='')
+    print("\033[2J",sep='',end='',flush=True)
 
 def showcursor():
-    print("\033[?25h",end='')
+    print("\033[?25h",sep='',end='',flush=True)
 
 def hidecursor():
-    print("\033[?25l",end='')
+    print("\033[?25l",sep='',end='',flush=True)
 
 def foreground(color):
-    print("\033[3",color,"m",end='')
+    print("\033[3",color,"m",sep='',end='',flush=True)
 
 def background(color):
-    print("\033[4",color,"m",end='')
+    print("\033[4",color,"m",sep='',end='',flush=True)
 
 def gotoxy(x,y):
-    print("\033[",x,";",y,"H",end='')
+    print("\033[",y,";",x,"H",sep='',end='',flush=True)
+
+def sleep(sec):
+    time.sleep(sec)
 
 def putchar(ch):
     if(len(ch) == 1):
-        print(ch,end='')
+        print(ch,sep='',end='',flush=True)
